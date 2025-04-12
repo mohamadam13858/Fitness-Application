774b6eadd276bfcea866c708f777a06d5823af56
mohamad <mh711748@gmail.com>
2025-04-12 15:42:50 +0330

    button

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from "@angular/core";
 import { MatSlideToggleModule } from "@angular/material/slide-toggle";
-
+import { MatButtonModule } from '@angular/material/button'
+import { MatIconModule } from '@angular/material/icon'
 @NgModule({
     imports: [
-        MatSlideToggleModule
-    ] , 
+        MatSlideToggleModule,
+        MatButtonModule,
+        MatIconModule
+    ],
     exports: [
-        MatSlideToggleModule
+        MatSlideToggleModule,
+        MatButtonModule,
+        MatIconModule
     ]
 })
 

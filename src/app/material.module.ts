d32dfef13462a70b6a509b721c3dab129f1683e2
mohamad <mh711748@gmail.com>
2025-04-12 22:57:08 +0330

    hint Anderror

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -2,18 +2,20 @@ import { NgModule } from "@angular/core";
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import {MatButtonModule} from '@angular/material/button';
-
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations'
 @NgModule({
     imports: [
         MatFormFieldModule,
         MatInputModule,
-        MatButtonModule
+        MatButtonModule,
+        BrowserAnimationsModule
 
     ],
     exports: [
         MatFormFieldModule,
         MatInputModule,
-        MatButtonModule
+        MatButtonModule,
+        BrowserAnimationsModule
     ]
 })
 

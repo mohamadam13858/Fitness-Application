51670499b1df20e3c9a134790c04b09a157a0b05
mohamad <mh711748@gmail.com>
2025-04-13 00:01:31 +0330

    Datepicker

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,21 +1,27 @@
 import { NgModule } from "@angular/core";
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
-import {MatButtonModule} from '@angular/material/button';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations'
+import { MatButtonModule } from '@angular/material/button';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+import { MatDatepickerModule } from '@angular/material/datepicker'
+import {MatNativeDateModule} from '@angular/material/core'
 @NgModule({
     imports: [
+        BrowserAnimationsModule,
         MatFormFieldModule,
         MatInputModule,
         MatButtonModule,
-        BrowserAnimationsModule
+        MatDatepickerModule , 
+        MatNativeDateModule
 
     ],
     exports: [
+        BrowserAnimationsModule,
         MatFormFieldModule,
         MatInputModule,
         MatButtonModule,
-        BrowserAnimationsModule
+        MatDatepickerModule,
+        MatNativeDateModule
     ]
 })
 

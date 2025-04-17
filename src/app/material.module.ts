8a8c203d57407f741c1f1caf171da8844bb63cde
mohamad <mh711748@gmail.com>
2025-04-17 16:04:20 +0330

    mohamad

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -18,7 +18,7 @@ import { MatDialogModule } from '@angular/material/dialog';
 import { MatTableModule } from '@angular/material/table';
 import { MatSortModule } from '@angular/material/sort'
 import { MatPaginatorModule } from '@angular/material/paginator'
-
+            
 @NgModule({
     imports: [
         BrowserAnimationsModule,
